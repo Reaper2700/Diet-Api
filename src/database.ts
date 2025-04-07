425a99c8a2ee8feb7dcd0bd09d9e912c05cd1d2d
Reaper2700 <rafaell_b.p@edu.unifor.br>
2025-04-07 17:07:43 -0300

    environment

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,15 @@
 import { Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
 // entender o knex config
 
 export const config: Knex.Config = {
-  client: 'sqlite3',
-  connection: {
-    filename: './db/app.db',
-  },
+  client: env.DATABASE_CLIENT,
+  connection: 
+  env.DATABASE_CLIENT === 'sqlite'
+  ? {
+      filename: env.DATABASE_URL,
+    }
+  : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
